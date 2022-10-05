aab7be80dadf9cfa8cbb5f38fb18c8593c812fcf
PotatoLife <esl112999@gmail.com>
2022-10-05 16:55:23 +0900

    Update JSONManager.swift

--- JSONParser/JSONManager.swift
+++ JSONParser/JSONManager.swift
@@ -13,50 +13,53 @@ import Alamofire
 
 /*
  struct Test: Codable {
-     let userId: Int
-     let id: Int
-     let title: String
-     let completed: Bool
-     
-     enum Codingkeys: String, CodingKey {
-         case userId, id, title, completed
-     }
-     
+ let userId: Int
+ let id: Int
+ let title: String
+ let completed: Bool
+ 
+ enum Codingkeys: String, CodingKey {
+ case userId, id, title, completed
  }
-
+ 
+ }
+ 
  class ViewController: UIViewController {
-     
-     private let jsonManager = JSONManager.shared
-     
-     override func viewDidLoad() {
-         super.viewDidLoad()
-         
-         jsonManager.parsing(urlString: "https://jsonplaceholder.typicode.com/todos/1", type: Test.self) { result, model in
-             if let model = model {
-                 print(model)
-             }
-         }
-     }
-     
+ 
+ private let jsonManager = JSONManager.shared
+ 
+ override func viewDidLoad() {
+ super.viewDidLoad()
+ 
+ jsonManager.parsing(urlString: "https://jsonplaceholder.typicode.com/todos/1", type: Test.self) { result, model in
+ if let model = model {
+ print(model)
+ }
+ }
+ }
+ 
  }
  */
+
 
 open class JSONManager {
     public static let shared = JSONManager()
     
-    func parsing<T: Codable>(urlString: String, type: T.Type, completion: @escaping (Bool, T?) -> Void){
+    func parsing<T: Codable>(httpMethod: HTTPMethod?, urlString: String, type: T.Type, completion: @escaping (Bool, T?) -> Void){
         guard let url = URL(string: urlString) else {
             completion(false, nil)
             return
         }
         
-        AF.request(url)
+        AF.request(url, method: httpMethod ?? .get)
             .validate(statusCode: 200..<500)
             .responseDecodable(of: T.self) { AFResponse in
                 if let data = AFResponse.value {
                     completion(true, data)
+                } else {
+                    completion(false, nil)
                 }
-        }
+            }
     }
-
+    
 }
